a15ed48c9c3fda9ee0e0e80b719f5d707a14a1ba
NotAnEnergyDrinkAddict <215243710+notanenergydrinkaddict@users.noreply.github.com>
2025-09-25 14:11:13 +1200

    Fix `CMakeLists` for CMake 4.0
    ` ${CMAKE_SOURCE_DIR}` for `target_include_directories` points to `Pencil4LineForBlender` at least on win32 and external dependencies are not resolved.
    `add_custom_command(TARGET Pencil4LineForBlender POST_BUILD...` will create invalid `bin` file (is target binary, not folder). this will be resolved later.

---
+++ Pencil4LineForBlender/makeCommitHash.bat
@@ -0,0 +1,4 @@
+mkdir ../Package/bin
+for /f "usebackq tokens=*" %%i in (`git log -n 1 --pretty^=format:"%%H" -- . ../blender ../cereal ../pybind11 ../pencil4line_for_blender_mac`) do @set hash=%%i
+echo %hash% > ../Package/bin/commitHash.txt
+echo #define GIT_COMMIT_HASH "%hash%" > commitHash.h
