3b83e47b457dccae67844b7925efe6c06f19a36b
NotAnEnergyDrinkAddict <215243710+notanenergydrinkaddict@users.noreply.github.com>
2025-09-25 15:06:43 +1200

    Add CMake install target

--- Pencil4LineForBlender/makeCommitHash.bat
+++ Pencil4LineForBlender/makeCommitHash.bat
@@ -1,4 +1,3 @@
-mkdir ../Package/bin
 for /f "usebackq tokens=*" %%i in (`git log -n 1 --pretty^=format:"%%H" -- . ../blender ../cereal ../pybind11 ../pencil4line_for_blender_mac`) do @set hash=%%i
-echo %hash% > ../Package/bin/commitHash.txt
+echo %hash% > commitHash.txt
 echo #define GIT_COMMIT_HASH "%hash%" > commitHash.h
